fe8af62d20dc2f52418050de867ba73ec5377b14
3rd <3rd@users.noreply.github.com>
2023-09-13 23:30:29 +0300

    chore: add playground command

--- tree-sitter-syslang/flake.nix
+++ tree-sitter-syslang/flake.nix
@@ -4,25 +4,22 @@
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
-      flake = false;
+      flake = true;
     };
   };
   outputs = { nixpkgs, flake-utils, ... }@inputs:
     let
       defaultPackage = pkgs:
         pkgs.callPackage
-          (nixpkgs + "/pkgs/development/tools/parsing/tree-sitter/grammar.nix")
-          { }
-          {
-            language = "syslang";
-            source = ./.;
-            inherit (pkgs.tree-sitter) version;
-          };
-    in
-    (flake-utils.lib.eachDefaultSystem (system:
+        (nixpkgs + "/pkgs/development/tools/parsing/tree-sitter/grammar.nix")
+        { } {
+          language = "syslang";
+          source = ./.;
+          inherit (pkgs.tree-sitter) version;
+        };
+    in (flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
-      in
-      {
+      in {
         defaultPackage = defaultPackage pkgs;
         devShell = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
@@ -31,8 +28,6 @@
             tree-sitter
           ];
         };
-      })) // (
-      let pkgs = import nixpkgs { };
-      in { defaultPackage = defaultPackage pkgs; }
-    );
+      })) // (let pkgs = import nixpkgs { };
+      in { defaultPackage = defaultPackage pkgs; });
 }
